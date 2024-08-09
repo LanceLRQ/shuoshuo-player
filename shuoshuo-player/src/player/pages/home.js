87f32e229a48f83598604ceeb7afb092892d910a
LanceLRQ <lancelrq@gmail.com>
2024-08-09 10:07:25 +0800

    add carousel

--- shuoshuo-player/src/player/pages/home.js
+++ shuoshuo-player/src/player/pages/home.js
@@ -4,10 +4,10 @@ import VideoAlbumCarousel from "@player/components/carousel";
 
 const HomePage = () => {
     return <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} xl={8}>
             <VideoAlbumCarousel />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={6} xl={4}>
 
         </Grid>
     </Grid>
