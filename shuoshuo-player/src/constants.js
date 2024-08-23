50e8cea9d4bd1773512a8d8489147bd2a73de1d3
LanceLRQ <lancelrq@gmail.com>
2024-08-23 17:58:55 +0800

    加入切片man的信息

--- shuoshuo-player/src/constants.js
+++ shuoshuo-player/src/constants.js
@@ -26,3 +26,21 @@ export const StartupLoadingTip = [
     '不听话',
     '蜀道山'
 ]
+
+export const SlicerHuman = [
+    {
+        name: '快进到什么破游戏',
+        mid: '1679379',
+        face: 'https://i0.hdslb.com/bfs/face/444140b8adc15d4f586585b32a6362c8716dc082.jpg@240w_240h_1c_1s_!web-avatar-space-header.avif'
+    },
+    {
+        name: '乂龘',
+        mid: '316443021',
+        face: 'https://i0.hdslb.com/bfs/face/147fdbbb3ef27c14f87ff733c1a8e9a70b01b01f.jpg@240w_240h_1c_1s_!web-avatar-space-header.avif'
+    },
+    {
+        name: '矢野綾音おと',
+        mid: '329057087',
+        face: 'https://i2.hdslb.com/bfs/face/dcd743648150e5c71216b4ee25def1b95076d610.jpg@240w_240h_1c_1s_!web-avatar-space-header.avif'
+    },
+]
--- shuoshuo-player/src/player/pages/discovery.js
+++ shuoshuo-player/src/player/pages/discovery.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import {Avatar, Box, Paper, IconButton, InputBase, Typography, Stack, Button} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import {SlicerHuman} from "@/constants";
 
 const DiscoveryPage = () => {
-    return <div></div>;
+    const [mode, setMode] = React.useState('index');
+    return <Box className={`player-discovery-main ${mode === 'search' ? 'mode-search' : ''}`}>
+        <Box className="discovery-search-bar">
+            <Paper
+                className="discovery-search-box"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+            >
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="请输入视频关键字"
+                    inputProps={{ 'aria-label': '请输入视频关键字' }}
+                />
+                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+        </Box>
+        <Box className="discovery-slicer-human">
+            <Box className="slicer-box-title"><Typography  variant="h6">切片Man</Typography></Box>
+            <Box className="discovery-slicer-human-container">
+                {SlicerHuman.map((item) => {
+                    return <Box className="slicer-card" key={item.mid}>
+                        <Avatar className="slicer-avatar" sx={{ width: 64, height: 64 }} src={item.face} />
+                        <Box className="slicer-meta">
+                            <Typography className="slicer-name" variant="h7">{item.name}</Typography>
+                            <Stack direction="row" spacing={2}>
+                                <Button>添加歌单</Button>
+                                <Button onClick={() => window.open(`https://space.bilibili.com/${item.mid}`)}>去TA空间</Button>
+                            </Stack>
+                        </Box>
+                    </Box>;
+                })}
+            </Box>
+        </Box>
+        <Box className="discovery-search-list">
+
+        </Box>
+    </Box>;
 }
 
 export default DiscoveryPage;
